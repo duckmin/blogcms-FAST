aeb583c908c14a2ffbf1f2f04006020cd3c29a26
duckmin <allthingsrobert@gmail.com>
2015-09-27 23:23:00 -0400

    add test layout for blocks under post

---
+++ main/test-layout.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<style type="text/css">
+	   ul {
+	       list-style-type: none;
+	       width:50%;
+	   }
+	   
+	   li {
+	       display:inline-block;
+	       width:50%;
+	       vertical-align: top;
+	   }
+	   
+	   ul li div {
+	       background-color: orange;
+	       height:200px;
+	       
+	   }
+	           
+	           
+	</style>
+	</head>
+	<body>
+		<ul>
+		    <li>
+                <div style="height:200px;background-color:red" ></div>
+		        <div style="height:300px;background-color:orange" ></div>
+		    </li><!--
+		    --><li>
+		        <div style="height:150px;background-color:blue" ></div>
+		        <div style="height:250px;background-color:green" ></div>
+		    </li>
+		</ul>
+	</body>
+</html>
