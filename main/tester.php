c074d36117025a3d3f31399430efdb8a05ec62eb
duckmin <allthingsrobert@gmail.com>
2015-09-30 14:58:43 -0400

    tester.php test for getting post preview text from post and not description, this is experimental

--- main/tester.php
+++ main/tester.php
@@ -2,8 +2,38 @@
 	$server = dirname(__FILE__)."/../server";
 	
 	include_once $server."/configs.php";
-	$db = MongoConnection();//file:///home/duckmin/projects/js_libs/libs_in_dev/extender_test.html
-
+	$db = MongoConnection();
+	$id = new MongoId( "56056409ce95181c26aa05db" );  //long post with plenty of text
+	$collection = $db->blog->posts;					
+	$item = $collection->findOne(array('_id'=>$id));
+	//echo var_dump( $item );
+	$post_data = $item["post_data"];
+	//echo var_dump( $post_data )."<br><br>";
+	
+	
+	function getPreviewTextFromMarkdown( $post_data_array ){
+		$preview = "";
+		foreach( $post_data_array as $post_item ){
+			
+			$post_type = $post_item["data-posttype"];
+			if( $post_type === "markdown" ){
+				 //echo var_dump( $post_item )."<br><br>";
+				 $word_matches = array();
+				 //"/\b[\w\d\']+\b(\,|\.|\'|!|\?|)/"
+				 preg_match_all( "/^\s*[^>!].+$/m", trim( $post_item["text"] ), $word_matches );
+				 echo var_dump( $word_matches[0] )."<br><br>";
+				 //$tmp_str = implode(" ", $word_matches[0]);
+				 //echo $tmp_str."<br><br>";
+				 //if( isset($hash_matches[1]) ){ 
+				 //	 $hashes = array_merge ( $hashes, array_map("strtolower", $hash_matches[1]) );  //lower case all hashes
+				 //}
+			}
+		}
+		//return $preview;
+	}
+	
+	getPreviewTextFromMarkdown( $post_data );	
+	
 	
 	//TEST TO GET TIMESTAMP OF ONE POST AND GET THE NEXT POST BACK IN TIME
 	//$time = 1427140819000/1000;	
